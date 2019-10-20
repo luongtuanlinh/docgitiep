6fb82c31d65b2031d720c73cc070e4ffc38263ec
anhkhoa45 <khoa.1997a@gmail.com>
2019-10-20 18:48:36 +0700

    Remove turbolinks (2019 bug)

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :recipes, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :recipes, only: [:index, :show]
 
   get '/', action: :index, controller: 'homepage'
   get 'single', action: :single, controller: 'homepage'
