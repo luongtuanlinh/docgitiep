7447eb1c3a5c03d7f5c42349b2776d1dbb421433
anhkhoa45 <khoa.1997a@gmail.com>
2019-10-20 18:05:53 +0700

    recipe show

--- app/test/controllers/recipes_controller_test.rb
+++ app/test/controllers/recipes_controller_test.rb
@@ -10,39 +10,8 @@ class RecipesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get new" do
-    get new_recipe_url
-    assert_response :success
-  end
-
-  test "should create recipe" do
-    assert_difference('Recipe.count') do
-      post recipes_url, params: { recipe: {  } }
-    end
-
-    assert_redirected_to recipe_url(Recipe.last)
-  end
-
   test "should show recipe" do
     get recipe_url(@recipe)
     assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_recipe_url(@recipe)
-    assert_response :success
-  end
-
-  test "should update recipe" do
-    patch recipe_url(@recipe), params: { recipe: {  } }
-    assert_redirected_to recipe_url(@recipe)
-  end
-
-  test "should destroy recipe" do
-    assert_difference('Recipe.count', -1) do
-      delete recipe_url(@recipe)
-    end
-
-    assert_redirected_to recipes_url
   end
 end
