e4ac9cc8f2450a20c9d4533bebf14d7e76add07a
anhkhoa45 <khoa.1997a@gmail.com>
2019-10-26 19:04:36 +0700

    Search

--- app/app/controllers/recipes_controller.rb
+++ app/app/controllers/recipes_controller.rb
@@ -5,7 +5,21 @@ class RecipesController < ApplicationController
   # GET /recipes.json
   def index
     page = params[:page] ? params[:page] : 1
-    @recipes = Recipe.order(created_at: :desc).page page
+
+    categories = category_search
+    ingredients = ingredient_search
+    recipes = recipe_search
+
+    @search_query = search_query
+    @recipe_hash = {
+      category: categories.nil? ? 
+                nil :
+                Recipe.where(category_id: categories.pluck(:id)).order(created_at: :desc).page(page).per(4),
+      ingredient: ingredients.nil? ? 
+                  nil :
+                  Recipe.joins(:ingredients).where(ingredients: {id: ingredients.pluck(:id)}).order(created_at: :desc).page(page).per(4),
+      recipe: recipes.nil? ? nil : recipes.page(page).per(4)
+    }
   end
 
   # GET /recipes/1
@@ -13,14 +27,51 @@ class RecipesController < ApplicationController
   def show
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_recipe
-      @recipe = Recipe.find(params[:id])
-    end
+private
+  def search_query
+    params[:query]
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def recipe_params
-      params.fetch(:recipe, {})
+  def search_type
+    params[:type]
+  end
+
+  def category_search
+    return nil if search_type.present? && search_type != 'category'
+    
+    if search_query.present?
+      return Category.where('name LIKE ?', "%#{search_query}%")
+    else
+      return nil
     end
+  end
+
+  def ingredient_search
+    return nil if search_type.present? && search_type != 'ingredient'
+    
+    if search_query.present?
+      return Ingredient.where('name LIKE ?', "%#{search_query}%")
+    else
+      return nil
+    end
+  end
+
+  def recipe_search
+    return nil if search_type.present? && search_type != 'recipe'
+    if search_query.present?
+      return Recipe.where('name LIKE ?', "%#{search_query}%")
+    else
+      return Recipe.all
+    end
+  end
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def recipe_params
+    params.fetch(:recipe, {})
+  end
 end
