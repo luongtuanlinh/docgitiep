0a8e4e5f775bc760ef2ebbfdc335fc372b90db6a
anhkhoa45 <khoa.1997a@gmail.com>
2019-11-06 15:49:47 +0000

    Ignore vendor

--- app/app/models/comment.rb
+++
@@ -1,5 +0,0 @@
-class Comment < ApplicationRecord
-    belongs_to :recipe
-    belongs_to :user 
-    has_many :replies
-end
--- app/app/models/recipe.rb
+++
@@ -1,7 +0,0 @@
-class Recipe < ApplicationRecord
-    belongs_to :category
-    has_many :ingredients
-    has_many :comments
-    
-    paginates_per 6
-end
